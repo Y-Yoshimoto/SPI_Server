657a15377a1d0a68175dea749323bc070752486f
Y-Yoshimoto <mewyoshimoto@gmail.com>
2018-02-04 23:59:54 +0900

    modified:   AP/install.sh

--- AP/install.sh
+++ AP/install.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # --name, --disk, --locationを編集
-qemu-img create -f qcow2 '~/SPIArea/KVM_images/APServer_1.qcow2' 5G
+qemu-img create -f qcow2 '$HOME/SPIArea/KVM_images/APServer_1.qcow2' 5G
 virt-install \
 --name APServer_1 \
 --ram=640 \
---disk path='~/SPIArea/KVM_images/APServer_1.qcow2',size=10,format=qcow2 \
+--disk path='$HOME/SPIArea/KVM_images/APServer_1.qcow2',size=10,format=qcow2 \
 --vcpus 1 \
 --os-type linux \
 --os-variant rhel7 \
 --network network=default \
 --graphics none \
 --console pty,target_type=serial \
---location '~/SPIArea/OS_iso/CentOS-7-x86_64-Minimal-1708.iso' \
+--location '$HOME/SPIArea/OS_iso/CentOS-7-x86_64-Minimal-1708.iso' \
 --extra-args 'console=ttyS0,115200n8 serial'
