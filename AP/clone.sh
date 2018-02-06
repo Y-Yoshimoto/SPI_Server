3ba010f4b2ed449f7f441c402032db50eabfda06
Y-Yoshimoto <mewyoshimoto@gmail.com>
2018-02-07 00:36:25 +0900

    KVMクローンスクリプト更新 コピー元及びコピー先を引数で指定

--- AP/clone.sh
+++ AP/clone.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
+# ./clone.sh OriginalName　CopyName
+echo "Original:" $1
+echo "Copy to :" $2
+
+# Confirm the number of arguments.
+if [ $# -ne 2 ]; then
+    echo "Two arguments are required." 1>&2
+    exit 1
+fi
+
+# Yes or no
+echo "Do you want to clone with this setting [y/n]?"
+read ANSWER
+case $ANSWER in
+    "y" | "yes") echo "yes";;
+    * ) echo "no";exit 1;;
+esac
+
+# Clone of KVM virtual machine.
 virt-clone  \
---original Server1 \
---name Server2 \
---file '~/SPIArea/KVM_images/server.qcow2'
+--original $1 \
+--name $2 \
+--file "$HOME/SPIArea/KVM_images/"$2".qcow2"
+
+echo "End script"
