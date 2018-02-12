c8f01635486ec3d3feef4079f1d46e5808c3f62d
Y-Yoshimoto <mewyoshimoto@gmail.com>
2018-02-13 02:58:36 +0900

    Inport用のクエリを更新

--- DB/MakeTestDB/CopyFrom.sql
+++ DB/MakeTestDB/CopyFrom.sql
@@ -1,2 +1,1 @@
--- \copy t_postcode (id,JLG_Code,old_postcade,postcode,prefectural_kana,municipality_kana,town_kana,prefectural,municipality,town) FROM '/home/y-yoshimoto/okinawa.csv' WITH CSV;
-\copy t_postcode FROM '/$HOME/SPIArea/SPI_Server/DB/MakeTestDB/miniPostCodeEdit.csv' WITH CSV;
+load data local infile '~/miniPostCodeEdit.csv' into table t_postcode fields terminated by ',';
